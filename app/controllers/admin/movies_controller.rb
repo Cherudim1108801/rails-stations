507596d4556a185a3109e1f3d46eddcc092d9c83
KOHEI-ThinkPadX240 <tomikou11088011@gmail.com>
2022-04-30 15:40:46 +0900

    Station3とりあえず作成

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -4,6 +4,23 @@ class Admin::MoviesController < ApplicationController
   end
 
   def new
-    @Movie = Movie.new
+    @movie = Movie.new
   end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to admin_movies_path
+    else
+      flash[:alert] = "エラー"
+      redirect_to adomin_movies_new_path
+    end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:id, :name, :year, :description, :image_url, :is_showing, :created_at, :updated_at)
+    end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+    validates :id, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true
 end
