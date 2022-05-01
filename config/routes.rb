1127758756396af7be6d9ecc4134ed95fbf3cc65
KOHEI-ThinkPadX240 <tomikou11088011@gmail.com>
2022-05-01 19:12:34 +0900

    名前空間ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/movies", to: "movies#index"
 
-  get "/admin/movies", to: "admin/movies#adminindex"
+  namespace :admin do
+    get "/movies", to: "movies#adminindex"
   
-  get "/admin/movies/new", to: "admin/movies#new"
-  post "/admin/movies", to: "admin/movies#create"
-  
+    get "/movies/new", to: "movies#new"
+    post "/movies", to: "movies#create"
+  end
+
 end
