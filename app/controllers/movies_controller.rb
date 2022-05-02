6ced733587bee1f5e31a605faa7460460b4402d9
KOHEI-ThinkPadX240 <tomikou11088011@gmail.com>
2022-05-02 10:14:22 +0900

    通常のコントローラに書く？

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,21 @@ class MoviesController < ApplicationController
   def list
     @movies = Movie.all
   end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.valid?
+      redirect_to "/admin/movies"
+    else
+      flash[:alert] = "エラー"
+      render :new
+    end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:id, :name, :year, :description, :image_url, :is_showing, :created_at, :updated_at)
+    end
+
 end
