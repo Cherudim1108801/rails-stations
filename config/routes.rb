240e89a42c29881200ae9cf5ccf1844619007ee6
KOHEI-ThinkPadX240 <tomikou11088011@gmail.com>
2022-05-02 12:17:50 +0900

    Station02確認

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/movies", to: "movies#list"
 
-  get "/admin/movies", to: "admin/movie#index"
+  get "/admin/movies", to: "admin/movies#index"
 
-  get "/admin/movies/new", to: "admin/movie#new"
-  post "/admin/movies", to: "admin/movie#create"
+  get "/admin/movies/new", to: "admin/movies#new"
+  post "/admin/movies", to: "admin/movies#create"
 
 end
