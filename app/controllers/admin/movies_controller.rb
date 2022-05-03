687284c139059a3f20054ba0ac90d0a22a6f7ca1
KOHEI-ThinkPadX240 <tomikou11088011@gmail.com>
2022-05-03 11:18:51 +0900

    Station04完成

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,26 +1,41 @@
 class Admin::MoviesController < ApplicationController
-    def index
-      @movies = Movie.all
-    end
+  def index
+    @movies = Movie.all
+  end
 
-    def new
-      @movie = Movie.new
-    end
+  def new
+    @movie = Movie.new
+  end
 
-    def create
-      @movie = Movie.new(movie_params)
+  def create
+    @movie = Movie.new(movie_params)
     
-      if @movie.save
-        redirect_to "/admin/movies"
-      else
-        flash[:alert] = "エラー"
-        render :new
-      end
+    if @movie.save
+      redirect_to "/admin/movies"
+    else
+      flash[:alert] = "エラー"
+      render :new
     end
+  end
+
+  def edit
+    @movie = Movie.find(movie_params[:id])
+  end
+
+  def update
+    @movie = Movie.find(movie_params[:id])
     
-    private
-    def movie_params
-      params.require(:movie).permit(:id, :name, :year, :description, :image_url, :is_showing, :created_at, :updated_at)
+    if @movie.save
+      redirect_to "/admin/movies"
+    else
+      flash[:alert] = "エラー"
+      render :edit
     end
-        
+  end
+
+  private
+  def movie_params
+    params.permit(:id, :name, :year, :description, :image_url, :is_showing, :created_at, :updated_at)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/movies", to: "movies#list"
 
-  get "/admin/movies", to: "admin/movies#index"
-
-  get "/admin/movies/new", to: "admin/movies#new"
-  post "/movies", to: "admin/movies#create"
-
-  get "/admin/movies/:id", to: "admin/movies#edit"
+  namespace :admin do
+    resources :movies
+  end
 
 end
