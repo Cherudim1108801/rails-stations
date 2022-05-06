bde89d64f697edafd24bf3304ba18c600c5cedd2
KOHEI-ThinkPadX240 <tomikou11088011@gmail.com>
2022-05-06 14:47:59 +0900

    Station06完成

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,23 @@ class MoviesController < ApplicationController
   def list
     @movies = Movie.all
   end
+
+  def search
+    showing = true
+    showing = params[:is_showing] == "0" ? false : true
+
+    if params[:keyword] == "" && params[:is_showing] == ""
+      @movies = Movie.all
+    elsif params[:is_showing] == ""
+      @movies = Movie.where("name LIKE ? OR description LIKE ?", "%#{params[:keyword]}%", "%#{params[:keyword]}%")
+    elsif params[:keyword] == ""
+      @movies = Movie.where(is_showing: showing)
+    else
+      @movies = Movie.where(is_showing: showing).and(Movie.where("name LIKE ? OR description LIKE ?", "%#{params[:keyword]}%", "%#{params[:keyword]}%"))
+    end
+
+    @is_showing = params[:is_showing]
+    @keyword = params[:keyword]
+    render :list
+  end
 end
